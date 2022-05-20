8bcc57a29935af8e5f83c6fd77574122ea7fe777
Abhishek Negi <nabhishek971@gmail.com>
2022-05-20 23:54:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const mongoose = require('mongoose');
+
+const homeStartingContent ="Blogs represent a sort of citizen media. They might be funded by a company or be the work and sole means of raising revenue for an individual blogger, but they remain far removed from the multi-billion dollar enterprises that are the traditional or mainstream media. When a blog reader has reason to distrust traditional sources of information, blogs fill the gap.";
+
+const aboutContent = "My name is Abhishek Negi. I'm currently pursuing BTech from Doon Institute of Engineering and Technology, Rishikesh, Uttarakhand.";
+
+const contactContent = "nabhishek971@gmail.com";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started");
+});
